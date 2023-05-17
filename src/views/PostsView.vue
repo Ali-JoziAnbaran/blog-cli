f827c4adf0865542a2a14ee8b17e96af2924a3ae
VahidJ ahangiri <vahidjahangir1998@gmail.com>
2023-05-17 09:28:18 +0330

    add sweetalert to project and fix edit

--- src/views/PostsView.vue
+++ src/views/PostsView.vue
@@ -1,171 +1,228 @@
 <template>
-  <div class="container-fluid">
-    <div class="col-md-12">
-      <div class="row">
-        <div class="col-md-3">
-          <SideBar />
-        </div>
-        <div class="col-md-9">
-          <div class="col-md-6">
-            <b-form @submit="onSubmit" @reset="onReset" enctype="multipart/form-data">
-              <b-form-group
-                label-cols="6"
-                label-cols-lg="3"
-                class="text-left"
-                label-size="sm"
-                label="Post Name:"
-                label-for="input-name"
-              >
-                <b-form-input
-                  id="input-name"
-                  size="sm"
-                  v-model="form.name"
-                ></b-form-input>
-                <span class="text-danger" v-if="errors.name">{{
-                  errors.name
-                }}</span>
-              </b-form-group>
-              <b-form-group
-                label-cols="6"
-                label-cols-lg="3"
-                class="text-left"
-                label-size="sm"
-                label="Slug:"
-                label-for="input-slug"
-              >
-                <b-form-input
-                  id="input-slug"
-                  size="sm"
-                  v-model.trim="form.slug"
-                ></b-form-input>
-                <span class="text-danger" v-if="errors.slug">{{
-                  errors.slug
-                }}</span>
-              </b-form-group>
-              <b-form-group
-                label="Image:"
-                label-cols-sm="3"
-                class="text-left"
-                label-size="sm"
-              >
-                <b-form-file
-                  id="file-small"
-                  size="sm"
-                  v-model="form.image"
-                  accept=".jpg, .png, .gif"
-                  :state="Boolean(form.image)"
-                ></b-form-file>
-              </b-form-group>
-              <b-form-group
-                label-cols="6"
-                label-cols-lg="3"
-                class="text-left"
-                label-size="sm"
-                label="Description:"
-                label-for="textarea-description"
-              >
-                <b-form-textarea
-                  id="textarea-description"
-                  size="sm"
-                  v-model="form.description"
-                ></b-form-textarea>
-                <span class="text-danger" v-if="errors.description">{{
-                  errors.description
-                }}</span>
-              </b-form-group>
+    <div class="container-fluid">
+        <div class="col-md-12">
+            <div class="row">
+                <div class="col-5">
+                    <b-form
+                        @submit="onSubmit"
+                        @reset="onReset"
+                        enctype="multipart/form-data"
+                    >
+                        <b-form-group
+                            label-cols="6"
+                            label-cols-lg="3"
+                            class="text-left"
+                            label-size="sm"
+                            label="Post Name:"
+                            label-for="input-name"
+                        >
+                            <b-form-input
+                                id="input-name"
+                                size="sm"
+                                v-model="form.name"
+                            ></b-form-input>
+                            <span class="text-danger" v-if="errors.name">{{
+                                errors.name
+                            }}</span>
+                        </b-form-group>
+                        <b-form-group
+                            label-cols="6"
+                            label-cols-lg="3"
+                            class="text-left"
+                            label-size="sm"
+                            label="Slug:"
+                            label-for="input-slug"
+                        >
+                            <b-form-input
+                                id="input-slug"
+                                size="sm"
+                                v-model.trim="form.slug"
+                            ></b-form-input>
+                            <span class="text-danger" v-if="errors.slug">{{
+                                errors.slug
+                            }}</span>
+                        </b-form-group>
+                        <b-form-group
+                            label-cols="6"
+                            label-cols-lg="3"
+                            class="text-left"
+                            label-size="sm"
+                            label="Description:"
+                            label-for="textarea-description"
+                        >
+                            <b-form-textarea
+                                id="textarea-description"
+                                size="sm"
+                                v-model="form.description"
+                            ></b-form-textarea>
+                            <span
+                                class="text-danger"
+                                v-if="errors.description"
+                                >{{ errors.description }}</span
+                            >
+                        </b-form-group>
 
-              <b-button type="submit" variant="primary" class="mr-3"
-                >Submit</b-button
-              >
-              <b-button type="reset" variant="danger">Reset</b-button>
-            </b-form>
-          </div>
+                        <b-button
+                            type="submit"
+                            variant="primary"
+                            class="mr-3"
+                            >{{ editId ? "Update" : "Submit" }}</b-button
+                        >
+                        <b-button type="reset" variant="danger">Reset</b-button>
+                    </b-form>
+                </div>
+                <div class="col-7">
+                    <b-table hover :items="table" :fields="tableCells">
+                        <template v-slot:cell(edit)="{ item }">
+                            <span
+                                ><b-btn
+                                    @click="editItem(item)"
+                                    variant="primary"
+                                    ><b-icon icon="pencil-fill"></b-icon></b-btn
+                            ></span>
+                        </template>
+                        <template v-slot:cell(delete)="{ table }">
+                            <span
+                                ><b-btn
+                                    @click="showDeletePopup(table.id)"
+                                    variant="danger"
+                                    ><b-icon icon="trash-fill"></b-icon></b-btn
+                            ></span>
+                        </template>
+                    </b-table>
+                </div>
+            </div>
         </div>
-      </div>
     </div>
-  </div>
 </template>
 
 <script>
-  import SideBar from "../components/SideBar.vue";
 
-  export default {
-    name: "PostsView",
-    components: {
-      SideBar,
-    },
-    data: function () {
-      return {
-        form: {
-          name: "",
-          slug: "",
-          image: null,
-          description: null,
+    export default {
+        name: "PostsView",
+        data: function () {
+            return {
+                form: {
+                    name: "",
+                    slug: "",
+                    description: null,
+                },
+                errors: {
+                    name: "",
+                    slug: "",
+                    description: "",
+                },
+                tableCells: ["id", "name", "description", "edit", "delete"],
+                table: [],
+                editId: "",
+                deleteId: "",
+            };
         },
-        errors: {
-          name: "",
-          slug: "",
-          description: "",
-        },
-      };
-    },
-    methods: {
-      onSubmit(event) {
-        event.preventDefault();
-        console.log(this.form.image);
-        if (!this.checkValidation(this.form)) return false;
-        const allPosts = localStorage.getItem("posts")
-          ? JSON.parse(localStorage.getItem("posts"))
-          : [];
-        allPosts.push({...this.form, id: allPosts.length + 1});
-        localStorage.setItem("posts", JSON.stringify(allPosts));
-      },
-      onReset(event) {
-        event.preventDefault();
-        this.form.name = "";
-        this.form.slug = "";
-        this.form.image = null;
-        this.form.description = "";
-      },
-      checkValidation(form) {
-        this.errors = {};
-        let returnForm = true;
-        if (!form.name) {
-          this.errors.name = "Name is required";
-          returnForm = false;
-        }
-        const allPosts = localStorage.getItem("posts")
-          ? JSON.parse(localStorage.getItem("posts"))
-          : null;
-        let findSlug = false;
-        if(allPosts) findSlug = allPosts.find((post) => post.slug === form.slug);
-        if (!form.slug) {
-          this.errors.slug = "Slug is required";
-          returnForm = false;
-        } else if (form.slug.indexOf(" ") >= 0) {
-          this.errors.slug = "Please enter the correct slug";
-          returnForm = false;
-        } else if (findSlug) {
-          this.errors.slug = "Slug already exists";
-          returnForm = false;
-        }
-        if (!form.description) {
-          this.errors.description = "Description is required";
-          returnForm = false;
-        }
+        methods: {
+            onSubmit(event) {
+                event.preventDefault();
+                if (!this.checkValidation(this.form)) return false;
+                const allPosts = localStorage.getItem("posts")
+                    ? JSON.parse(localStorage.getItem("posts"))
+                    : [];
+                if (this.editId) {
+                    const editRow = allPosts.find(
+                        (post) => post.id === this.editId
+                    );
+                    editRow.name = this.form.name;
+                    editRow.slug = this.form.slug;
+                    editRow.description = this.form.description;
 
-        return returnForm;
-      },
-    },
-    watch: {
-      "form.name": function (newVal) {
-        this.form.slug = newVal.replace(/\s+/g, "-").toLowerCase();
-      },
-    },
-  };
+                    this.form.name = "";
+                    this.form.slug = "";
+                    this.form.description = "";
+					this.makeToast('Post has been updated.', 'primary');
+                } else {
+					allPosts.push({ ...this.form, id: allPosts.length + 1 });
+					this.makeToast('Post has been added.', 'primary');
+                }
+                localStorage.setItem("posts", JSON.stringify(allPosts));
+                this.fetchTable();
+            },
+            onReset(event) {
+                event.preventDefault();
+                this.form.name = "";
+                this.form.slug = "";
+                this.form.description = "";
+            },
+            checkValidation(form) {
+                this.errors = {};
+                let returnForm = true;
+                if (!form.name) {
+                    this.errors.name = "Name is required";
+                    returnForm = false;
+                }
+                const allPosts = localStorage.getItem("posts")
+                    ? JSON.parse(localStorage.getItem("posts"))
+                    : null;
+                let findSlug = false;
+                if (allPosts)
+                    findSlug = allPosts.find((post) => post.slug === form.slug);
+                if (!form.slug) {
+                    this.errors.slug = "Slug is required";
+                    returnForm = false;
+                } else if (form.slug.indexOf(" ") >= 0) {
+                    this.errors.slug = "Please enter the correct slug";
+                    returnForm = false;
+                } else if (findSlug && findSlug.id !== this.editId) {
+                    this.errors.slug = "Slug already exists";
+                    returnForm = false;
+                }
+                if (!form.description) {
+                    this.errors.description = "Description is required";
+                    returnForm = false;
+                }
+
+                return returnForm;
+            },
+            editItem(row) {
+                const allPosts = JSON.parse(localStorage.getItem("posts"));
+                const editId = allPosts.findIndex((post) => post.id === row.id);
+
+                this.form.name = allPosts[editId].name;
+                this.form.slug = allPosts[editId].slug;
+                this.form.description = allPosts[editId].description;
+                this.editId = row.id;
+            },
+            fetchTable() {
+                const allPosts = localStorage.getItem("posts")
+                    ? JSON.parse(localStorage.getItem("posts"))
+                    : null;
+                const tablePosts = [];
+                if (allPosts) {
+                    allPosts.map((post) => {
+                        tablePosts.push({
+                            id: post.id,
+                            name: post.name,
+                            description: post.description,
+                        });
+                    });
+                }
+                this.table = tablePosts;
+            },
+            makeToast(text) {
+				this.$swal(text);
+
+            },
+        },
+        watch: {
+            "form.name": function (newVal) {
+                this.form.slug = newVal.replace(/\s+/g, "-").toLowerCase();
+            },
+        },
+        mounted() {
+            this.fetchTable();
+			this.makeToast('Post has been updated.');
+        },
+    };
 </script>
 <style scoped>
-  .row {
-    display: flex;
-  }
+    .row {
+        display: flex;
+    }
 </style>
